46d2f890b090e47aa3e87bf006e7ec5fa6f37ffa
Константин Маслак <goodspeet93@gmail.com>
2022-04-26 16:52:15 +0300

    Added hw-4

---
+++ ControlPanelTest.java
@@ -0,0 +1,32 @@
+class ControlPanelTest {
+    public static void main(String[] args) {
+        new ControlPanel().control();
+    }
+}
+class ControlPanel {
+    private Altimeter altimeter = new Altimeter();
+    private AirGauge airGauge = new AirGauge();
+
+    public void control() {
+        altimeter.toString();
+        airGauge.toString();
+    }
+}
+class Altimeter {
+
+    @Override
+    public String toString() {
+        System.out.println("Measure height");
+        return null;
+    }
+}
+
+class AirGauge {
+
+    @Override
+    public String toString() {
+        System.out.println("Measure air pressure");
+        return null;
+    }
+}
+
